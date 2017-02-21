a9850296b9c35f8fc83476fc37f3ecdacc0367e6
Uday Shankar Singh <udayshankar1306@gmail.com>
2017-02-21 18:44:17 +0530

    Update performance_strategies.py

--- performance_strategies.py
+++ performance_strategies.py
@@ -10,6 +10,10 @@ description - Configures the maximum size in bytes for a table that will be broa
               By setting this value to -1 broadcasting can be disabled. 
               Note that currently statistics are only supported for Hive Metastore tables 
               where the command ANALYZE TABLE <tableName> COMPUTE STATISTICS noscan has been run. 
+Han't tried it but since stats are only available in Hive, I am not sure, If I am not to test.
 
       
 2.
+I got this pretty interesting link - [http://blog.hydronitrogen.com/2016/05/13/shuffle-free-joins-in-spark-sql/], reagarding
+reducing shuffling and getting query execution plan.
+I'll update the result once I'll test it against 500GB of data set
